b9aeeb7cc523b62491c10345d0d4d7a49584c139
Arslan Doddamani <arslandoddamani11@gmail.com>
2024-12-07 17:46:37 +0530

    add the achievement section of index page

--- src/components/Home/Success.jsx
+++ src/components/Home/Success.jsx
@@ -1,43 +1,22 @@
 import React from "react";
 import { TiArrowRight } from "react-icons/ti";
+import collage from "./assets/achievementCollage.png"
 
 const Success = () => {
   return (
     <>
-      <div className="w-full h-[170vh] sm:h-[130vh] lg:h-[90vh] bg-red-800">
+      <div className="w-full ">
         <h1 className="font-bold text-4xl text-center lg:hidden">Our Achievements & Success</h1>
         <div className="lg:flex justify-center">
-          <div className="w-full lg:w-1/2 lg:h-[90vh]">
-          <div className="relative w-full h-[80vh] lg:h-full bg-green-300">
-              <img
-                className="absolute"
-                src="https://i.pinimg.com/236x/4c/a0/da/4ca0da0a27585d5986aec3a2a707942c.jpg"
-                alt=""
-              />
-              <img
-                className="absolute"
-                src="https://i.pinimg.com/236x/ef/91/b8/ef91b86b0fdae5434b6f9f9fb864f002.jpg"
-                alt=""
-              />
-              <img
-                className="absolute" 
-                src="https://i.pinimg.com/236x/8c/1a/34/8c1a34f8c8c369c75d0f2581688671c1.jpg"
-                alt=""
-              />
-              <img
-                className="absolute"
-                src="https://i.pinimg.com/236x/d8/26/8b/d8268bab19c7fdcab89b6b960ca701ec.jpg"
-                alt=""
-              />
-          </div>
-            
+          <div className="flex justify-center items-center w-full p-5 py-10 lg:w-1/2 ">
+              <img src={collage} className="" alt="images" />   
           </div>
 
           <div className="w-full lg:w-1/2 px-5 xl:pt-24 lg:py-10">
             <h1 className="hidden lg:block font-bold text-5xl">Our Achievements</h1>
             <h1 className="hidden lg:block font-bold text-5xl">& Success</h1>
 
-            <p className="w-full xl:w-3/4 lg:mt-14 text-justify">
+            <p className="w-full xl:w-3/4 lg:mt-14 lg:text-justify">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
